77fda665c14573f40890dcdd31814182596c6063
Rashy-hub <yagoubi.r@gmail.com>
2024-08-12 13:35:55 +0200

    add troll face near title

--- src/containers/header.jsx
+++ src/containers/header.jsx
@@ -4,7 +4,10 @@ const Header = () => {
   return (
     <header className="bg-rose-300  text-black py-4 shadow-md">
       <div className="container mx-auto flex justify-center items-center">
-        <h1 className="text-2xl font-bold text-center">😎Emoji Search😎</h1>
+        <h1 className="text-2xl font-bold text-center">
+          <img className="inline w-10" src="/icons8-troll-face-doodle-96.ico" alt="" />
+          Emoji Search <img className="inline w-10" src="/icons8-troll-face-doodle-96.ico" alt="" />
+        </h1>
       </div>
     </header>
   );
